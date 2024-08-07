4dd36fb5dc1bd8b99fa4b807f3510a93214a8661
shemzz <davidshemang@gmail.com>
2024-08-07 16:00:59 +0100

    fix register Service

--- src/modules/auth/registerAuthServices.ts
+++ src/modules/auth/registerAuthServices.ts
@@ -1,24 +1,29 @@
 import { container } from "tsyringe";
 import { AuthComponents } from "./constants/AuthComponents";
+import type { IEmailVerificationService } from "./interfaces/IEmailVerificationService";
+import type { ILoginService } from "./interfaces/ILoginService";
+import type { IPasswordService } from "./interfaces/IPasswordService";
+import type { IRegisterService } from "./interfaces/IRegisterService";
+import type { ITokenService } from "./interfaces/ITokenService";
 import { EmailVerificationService } from "./services/EmailVerificationService";
 import { LoginService } from "./services/LoginService";
 import { PasswordService } from "./services/PasswordService";
 import { RegisterService } from "./services/RegisterService";
 import { TokenService } from "./services/TokenService";
 
-container.register<RegisterService>(AuthComponents.RegisterService, {
+container.register<IRegisterService>(AuthComponents.RegisterService, {
 	useClass: RegisterService,
 });
-container.register<TokenService>(AuthComponents.TokenService, {
+container.register<ITokenService>(AuthComponents.TokenService, {
 	useClass: TokenService,
 });
-container.register<PasswordService>(AuthComponents.PasswordService, {
+container.register<IPasswordService>(AuthComponents.PasswordService, {
 	useClass: PasswordService,
 });
-container.register<LoginService>(AuthComponents.LoginService, {
+container.register<ILoginService>(AuthComponents.LoginService, {
 	useClass: LoginService,
 });
-container.register<EmailVerificationService>(
+container.register<IEmailVerificationService>(
 	AuthComponents.EmailVerificationService,
 	{
 		useClass: EmailVerificationService,
--- src/modules/user/registerUserServices.ts
+++ src/modules/user/registerUserServices.ts
@@ -1,16 +1,19 @@
 import { container } from "tsyringe";
 
 import { UserComponents } from "./constants/UserComponents";
+import type { IDeleteUserService } from "./interface/IDeleteUserService";
+import type { IGetUserService } from "./interface/IGetUserService";
+import type { IUpdateUserService } from "./interface/IUpdateUserService";
 import { DeleteUserService } from "./services/DeleteUser.service";
 import { GetUserService } from "./services/GetUser.Service";
 import { UpdateUserService } from "./services/UpdateUser.service";
 
-container.register<UpdateUserService>(UserComponents.UpdateUserService, {
+container.register<IUpdateUserService>(UserComponents.UpdateUserService, {
 	useClass: UpdateUserService,
 });
-container.register<GetUserService>(UserComponents.GetUserService, {
+container.register<IGetUserService>(UserComponents.GetUserService, {
 	useClass: GetUserService,
 });
-container.register<DeleteUserService>(UserComponents.DeleteUserService, {
+container.register<IDeleteUserService>(UserComponents.DeleteUserService, {
 	useClass: DeleteUserService,
 });
