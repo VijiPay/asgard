6398983ef186afaf8696b1daa9fb408bf94e3164
shemzz <davidshemang@gmail.com>
2024-08-15 15:22:56 +0100

    refactor Datasource

--- src/registerRepositories.ts
+++ src/registerRepositories.ts
@@ -1,7 +1,9 @@
-import { dataSource } from "./database/dataSource";
+import { PrismaClient } from "@prisma/client";
 import { registerAuthRepositories } from "./modules/auth/registerAuthRepositories";
 import { registerUserRepositories } from "./modules/user/registerUserRepositories";
 import type { ILogger } from "./shared/services/logger/ILogger";
+
+export const dataSource = new PrismaClient();
 
 export const registerRepositories = async (logger?: ILogger) => {
 	await dataSource
